323dd9b8b57c03bddf9f407d810925f759b1c310
damien.haynes <damien.haynes@8487f691-4123-8209-4799-60a5f0301f9a>
2011-10-13 06:34:56 +0000

    Added auto-increment of revision. Just need to modify the Minor/Major version after each release.

--- trunk/IMDb+/Properties/AssemblyInfo.cs
+++ trunk/IMDb+/Properties/AssemblyInfo.cs
@@ -23,18 +23,5 @@ using MediaPortal.Common.Utils;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0cbbc7e0-59f2-4989-93db-ee44b93c57cb")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
-
 // MediaPortal Version Compatibility
 [assembly: CompatibleVersion("1.1.6.27644")]
