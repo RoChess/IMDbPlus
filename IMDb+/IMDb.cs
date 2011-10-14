f5377c6616555210724273a3524de5714714e57e
Damien Haynes <damienhaynes@users.noreply.github.com>
2011-10-14 03:41:55 +0000

    Better checking and logging for replacements updates.

--- IMDb+/IMDb.cs
+++ IMDb+/IMDb.cs
@@ -539,19 +539,27 @@ namespace IMDb
                     GUIUtils.SetProperty("#IMDb.Scraper.LastUpdated", PluginSettings.SyncLastDateTime);
                 }
 
+                Logger.Info("Checking for replacements update");
+
                 // Update Replacements Database
                 localFile = GetTempFilename();
                 if (DownloadFile(ReplacementsUpdateFile, localFile))
                 {
-                    try
-                    { 
+                    // only update replacements if they differ
+                    if (!FilesAreEqual(localFile, ReplacementsFile))
+                    {
                         // replace existing file
                         Logger.Info("Updating Replacements Database");
-                        File.Copy(localFile, ReplacementsFile, true);
-   
-                        // remove temp download file
-                        File.Delete(localFile);
+                        try { File.Copy(localFile, ReplacementsFile, true); }
+                        catch { }
                     }
+                    else
+                    {
+                        Logger.Info("Skipping update, latest version already installed.");
+                    }
+
+                    // remove temp download file
+                    try { File.Delete(localFile); }
                     catch { }
                 }
 
@@ -780,5 +788,42 @@ namespace IMDb
             
             GUIWindowManager.Process();
         }
+
+        private bool FilesAreEqual(string f1, string f2)
+        {
+            // get file length and make sure lengths are identical
+            long length = new FileInfo(f1).Length;
+            if (length != new FileInfo(f2).Length)
+                return false;
+
+            byte[] buf1 = new byte[4096];
+            byte[] buf2 = new byte[4096];
+
+            // open both for reading
+            using (FileStream stream1 = File.OpenRead(f1))
+            using (FileStream stream2 = File.OpenRead(f2))
+            {
+                // compare content for equality
+                int b1, b2;
+                while (length > 0)
+                {
+                    // figure out how much to read
+                    int toRead = buf1.Length;
+                    if (toRead > length)
+                        toRead = (int)length;
+                    length -= toRead;
+
+                    // read a chunk from each and compare
+                    b1 = stream1.Read(buf1, 0, toRead);
+                    b2 = stream2.Read(buf2, 0, toRead);
+                    for (int i = 0; i < toRead; ++i)
+                        if (buf1[i] != buf2[i])
+                            return false;
+                }
+            }
+
+            return true;
+        }
+
     }
 }
